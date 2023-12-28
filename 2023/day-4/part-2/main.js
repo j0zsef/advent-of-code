6e20e2275f2b8a2ff388ac34b6d58456f18c222f
Joe Nevin <jnevin@enova.com>
2023-12-28 14:23:39 -0600

    Day4 Part2.

--- 2023/day-4/part-2/main.js
+++ 2023/day-4/part-2/main.js
@@ -9,29 +9,34 @@ function parsePuzzleInputAndSum(input) {
     const parsedInput = input.split('\n');
     const winningNumbers = [];
     const pickedNumbers = [];
-    let sum = 0;
+    const scratchCards = []
 
     for (let i = 0; i < parsedInput.length; i++) {
         let splitCard = parsedInput[i].split('|');
         pickedNumbers.push(splitCard[1].match(/\d+/g));
         winningNumbers.push(splitCard[0].split(':')[1].match(/\d+/g));
+        scratchCards.push(0);
     }
 
     for (let i = 0; i < winningNumbers.length; i++) {
         let winCount = 0;
         for (let j = 0; j < winningNumbers[i].length; j++) {
             if(pickedNumbers[i].indexOf(winningNumbers[i][j]) !== -1){
-                if (winCount === 0){
-                    winCount++;
-                }else {
-                    winCount *= 2;
+                winCount++;
+            }
+        }
+        scratchCards[i]++;
+        if (winCount > 0) {
+            let winLength = i + winCount
+            for (let j = 0; j < scratchCards[i]; j++) {
+                for (let k = i + 1; k <= winLength; k++) {
+                    scratchCards[k]++;
                 }
             }
         }
-        sum += winCount;
     }
 
-    return sum;
+    return scratchCards.reduce((acc, currentValue) => acc + currentValue);
 }
 
 console.log(parsePuzzleInputAndSum(readPuzzleInput()));
